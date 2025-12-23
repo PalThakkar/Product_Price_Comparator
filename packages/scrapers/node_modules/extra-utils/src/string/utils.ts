facaff8baf4c4fd4da3dc20c9928213e42da9f50
Pal <palthakkar24@gmail.com>
2025-12-23 11:54:49 +0530

    redis caching added

--- packages/scrapers/node_modules/extra-utils/src/string/utils.ts
+++
@@ -1,7 +0,0 @@
-export function isBlankLine(line: string): boolean {
-  return line.trim() === ''
-}
-
-export function isntBlankLine(line: string): boolean {
-  return !isBlankLine(line)
-}
--- packages/scrapers/node_modules/hotypes/src/unpacked-array-like.ts
+++
@@ -1,4 +0,0 @@
-export type UnpackedArrayLike<T> =
-  T extends ArrayLike<infer U>
-  ? U
-  : never
